34a8a0d4e47759b6ec8b5e4adbfccbff134059ad
Andreas <Andreas.Zwander@gmail.com>
2025-09-11 22:59:53 +0200

    Add `Read` and `Write` methods with `Span` support to `BaseTarFileStream`

---
+++ TarTools/Streams/Base/BaseTarFileStream.span.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TarTools.Streams.Base;
+
+public partial class BaseTarFileStream
+{
+#if NETCOREAPP2_1_OR_GREATER || NETSTANDARD2_1_OR_GREATER
+    public int Read(Span<byte> buffer)
+    {
+        ThrowIfDisposed();
+        return _stream.Read(buffer);
+    }
+
+    public void Write(ReadOnlySpan<byte> buffer)
+    {
+        ThrowIfDisposed();
+        _stream.Write(buffer);
+    }
+#endif
+}
