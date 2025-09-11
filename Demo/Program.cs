341e58d9de55f140c29c5bce5e04265efd995047
Andreas <Andreas.Zwander@gmail.com>
2025-09-12 01:47:11 +0200

    Integrate `TarFileStream` usage in `Program` for writing tar files and testing functionality

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,5 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using System;
+using System.Formats.Tar;
+using TarTools.Streams;
 
 Console.WriteLine("Hello, World!");
+var ms = new MemoryStream();
+var tarFileStream = new TarFileStream(ms, true);
+tarFileStream.WriteName("test");
+
+File.WriteAllBytes("test.tar", ms.ToArray());
