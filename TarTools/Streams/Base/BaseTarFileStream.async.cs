c8e8462e92a0064eb419f51c930bd95c80fb2c38
Andreas <Andreas.Zwander@gmail.com>
2025-09-11 22:49:14 +0200

    Add async disposal support to `BaseTarFileStream` targeting modern frameworks

---
+++ TarTools/Streams/Base/BaseTarFileStream.async.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace TarTools.Streams.Base;
+
+
+#if NETSTANDARD2_1_OR_GREATER || NETCOREAPP3_1_OR_GREATER
+public partial class BaseTarFileStream : IAsyncDisposable
+{
+    public async ValueTask DisposeAsync()
+    {
+        if (!_disposed)
+        {
+            if (!leaveStreamOpen)
+            {
+                if (_stream is IAsyncDisposable asyncDisposable)
+                    await asyncDisposable.DisposeAsync();
+                else
+                    _stream.Dispose();
+            }
+            _disposed = true;
+        }
+        GC.SuppressFinalize(this);
+    }
+}
+#endif
