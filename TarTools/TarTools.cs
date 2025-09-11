6499258ee0865f159fc68354b21b831827b61e69
Andreas <Andreas.Zwander@gmail.com>
2025-09-12 01:47:03 +0200

    Add `TarTools` class with `GetAsciiBytes` utility method

---
+++ TarTools/TarTools.cs
@@ -0,0 +1,8 @@
+﻿using System.Text;
+
+namespace TarTools;
+
+public static class TarTools
+{
+    public static byte[] GetAsciiBytes(string text) => Encoding.GetEncoding("ASCII").GetBytes(text);
+}
