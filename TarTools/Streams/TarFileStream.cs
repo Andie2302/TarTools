5351d1340d9d8ae3e9ef6a105e61a6a5266c2160
Andreas <Andreas.Zwander@gmail.com>
2025-09-12 02:14:11 +0200

    Enhance `WriteName` in `TarFileStream` to support padded ASCII writing with custom size and filler

--- TarTools/Streams/TarFileStream.cs
+++ TarTools/Streams/TarFileStream.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using TarTools.Streams.Base;
 
 namespace TarTools.Streams;
@@ -7,5 +8,13 @@ public class TarFileStream
 {
     private readonly BaseTarFileStream _base;
     public TarFileStream(Stream? stream, bool leaveOpen) => _base = new BaseTarFileStream(stream, leaveOpen);
-    public void WriteName(string name) => _base.Write(TarTools.GetAsciiBytes(name));
+
+    public void WriteName(string name, int size = 100, byte filler = 0x00)
+    {
+        var array = new byte[size];
+        TarTools.Fill(array, filler, 0, size);
+        var quelle = TarTools.GetAsciiBytes(name);
+        Buffer.BlockCopy(quelle, 0, array, 0, Math.Min(quelle.Length, array.Length));
+        _base.Write(array);
+    }
 }
