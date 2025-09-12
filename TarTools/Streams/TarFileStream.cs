b422fc90a4eef2ce03e8b994fc3b643cab519708
Andreas <Andreas.Zwander@gmail.com>
2025-09-12 03:40:27 +0200

    Refactor `TarFileStream` to add support for writing ASCII text, decimal, octal, and hex numbers with formatting options

--- TarTools/Streams/TarFileStream.cs
+++ TarTools/Streams/TarFileStream.cs
@@ -1,20 +1,27 @@
 ﻿using System;
 using System.IO;
 using TarTools.Streams.Base;
-
 namespace TarTools.Streams;
-
-public class TarFileStream
+public class TarFileStream(Stream? stream, bool leaveOpen)
 {
-    private readonly BaseTarFileStream _base;
-    public TarFileStream(Stream? stream, bool leaveOpen) => _base = new BaseTarFileStream(stream, leaveOpen);
-
-    public void WriteName(string name, int size = 100, byte filler = 0x00)
+    private readonly BaseTarFileStream _base = new(stream, leaveOpen);
+    public void WriteAsciiText(string name, int minimalTextLength = -1, byte filler = 0x00)
     {
-        var array = new byte[size];
-        TarTools.Fill(array, filler, 0, size);
-        var quelle = TarTools.GetAsciiBytes(name);
-        Buffer.BlockCopy(quelle, 0, array, 0, Math.Min(quelle.Length, array.Length));
+        var actualMinimalLength = minimalTextLength <= name.Length ? name.Length : minimalTextLength;
+        var array = new byte[actualMinimalLength];
+        TarTools.Fill(array, filler, 0, actualMinimalLength);
+        var sourceBytes = TarTools.GetAsciiBytes(name);
+        Buffer.BlockCopy(sourceBytes, 0, array, 0, Math.Min(sourceBytes.Length, array.Length));
         _base.Write(array);
     }
+    public void WriteAsciiDecimalNumber(long number, int minDigits = 0) => WriteFormattedNumber(number, minDigits, "D");
+    public void WriteAsciiHexNumber(long number, int minDigits = 0) => WriteFormattedNumber(number, minDigits, "X");
+    public void WriteAsciiOctalNumber(long number, int minDigits = 0)
+    {
+        var octalString = Convert.ToString(number, 8);
+        if (minDigits > 0)
+            octalString = octalString.PadLeft(minDigits, '0');
+        _base.Write(TarTools.GetAsciiBytes(octalString));
+    }
+    private void WriteFormattedNumber(long number, int minDigits, string format) => _base.Write(TarTools.GetAsciiBytes(number.ToString(minDigits > 0 ? $"{format}{minDigits}" : format)));
 }
