53e3937ed6d1921934fa732e6f72d8db723b4b8b
Andreas <Andreas.Zwander@gmail.com>
2025-09-12 02:14:15 +0200

    Add `Fill` method to `TarTools` for byte array manipulation

--- TarTools/TarTools.cs
+++ TarTools/TarTools.cs
@@ -1,8 +1,33 @@
-﻿using System.Text;
+﻿using System;
+using System.Text;
 
 namespace TarTools;
 
 public static class TarTools
 {
     public static byte[] GetAsciiBytes(string text) => Encoding.GetEncoding("ASCII").GetBytes(text);
+
+    public static byte[] Fill(byte[] array, byte b, int start, int length)
+    {
+        // Validierung der Eingabeparameter
+        if (array == null)
+            throw new ArgumentNullException(nameof(array));
+        
+        if (start < 0)
+            throw new ArgumentOutOfRangeException(nameof(start), "Start index cannot be negative");
+        
+        if (length < 0)
+            throw new ArgumentOutOfRangeException(nameof(length), "Length cannot be negative");
+        
+        if (start + length > array.Length)
+            throw new ArgumentOutOfRangeException(nameof(length), "Start index and length exceed array bounds");
+        
+        // Füllen des Arrays mit dem angegebenen Byte-Wert
+        for (var i = start; i < start + length; i++)
+        {
+            array[i] = b;
+        }
+
+        return array;
+    }
 }
